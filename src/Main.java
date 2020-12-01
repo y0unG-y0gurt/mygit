4ba9eb2465f648fd3e9b6b8ea5720a57b0640119
y0ung-y0gurt <peizc2019@gmail.com>
2020-12-01 14:01:24 +0800

    第三次提交2020.11.23 20：41

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] p = {1, 2, 3, 4, 5};
         swap(p,1,3);
-        System.out.println(p[1]);
+        System.out.println(Arrays.stream(p).max());
     }
 
     private static void swap(int[] p, int i, int i1) {
-        int tmp = p[i];
-        p[i] = p[i1];
-        p[i1] = tmp;
+//        int tmp = p[i];
+//        p[i] = p[i1];
+//        p[i1] = tmp;
+        p[i] = p[i] ^ p[i1];
+        p[i1] = p[i] ^ p[i1];
+        p[i] = p[i] ^ p[i1];
     }
 }
