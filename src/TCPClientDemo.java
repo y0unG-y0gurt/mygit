5fc2787c0be3c9c3e778a8d2c78e9957c9fddc98
y0ung-y0gurt <peizc2019@gmail.com>
2020-12-11 13:40:20 +0800

    第三次提交2020.11.23

--- src/TCPClientDemo.java
+++ src/TCPClientDemo.java
@@ -8,16 +8,34 @@ public class TCPClientDemo {
     public static void main(String[] args) throws UnknownHostException, IOException {
         // TODO Auto-generated method stub
         //1.建立TCP连接
-        String ip="10.105.53.65";   //服务器端ip地址
-        int port=10002;        //端口号
-        Socket sck=new Socket(ip,port);
+        String ip="192.168.229.1";   //服务器端ip地址
+        int port = 6000;//端口号
+        Socket[] sck = new Socket[3];
+        for(int i = 0;i < sck.length; i++){
+            sck[i] = new Socket(ip, port);
+        }
         //2.传输内容
-        String content="这是一个java模拟客户端";
+        String content="这是一个java模拟客户端\n";
         byte[] bstream=content.getBytes("GBK");  //转化为字节流
-        OutputStream os=sck.getOutputStream();   //输出流
-        os.write(bstream);
+        OutputStream[] os = new OutputStream[3];
+        for(int i = 0;i < sck.length; i++){
+            os[i]=sck[i].getOutputStream();   //输出流
+            os[i].write(bstream);
+        }
+        for(int i = 0; i < 10; i++){
+            os[0].write(bstream);
+            os[1].write(bstream);
+            os[2].write(bstream);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         //3.关闭连接
-        sck.close();
+        sck[0].close();
+        sck[1].close();
+        sck[2].close();
     }
 
 }
